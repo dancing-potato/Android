93e2b2e95ef5910087b450377454a45cf3270e6a
Zero Kim <105136541+dancing-potato@users.noreply.github.com>
2022-11-20 00:44:29 +0900

    1109_Layout_ScrollView

---
+++ And1109_Layout_ScrollView/app/src/main/java/com/example/and1109_layout_scrollview/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.and1109_layout_scrollview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.scrollview_test);
+    }
+}
