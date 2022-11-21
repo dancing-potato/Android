a80407b8ee2144fad754922a0d57d10dcc6e1bad
Zero Kim <105136541+dancing-potato@users.noreply.github.com>
2022-11-21 21:55:56 +0900

    1109_SlidingDrawer

---
+++ And1109_SlidingDrawer/app/src/main/java/com/example/and1109_slidingdrawer/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.and1109_slidingdrawer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
